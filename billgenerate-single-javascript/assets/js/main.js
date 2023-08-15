eef412316bbec7f127a2bc1225214c5ead07dfd2
Brayan Esteves <brayan.esteves93@gmail.com>
2023-08-15 09:55:23 -0400

    Bill Generate (Single) - JavaScript Vanilla (Amount) [14/08/2023]

--- billgenerate-single-javascript/assets/js/main.js
+++ billgenerate-single-javascript/assets/js/main.js
@@ -1,7 +1,7 @@
-let barbacued_chicken_price = 20, barbacued_chicken_quantity = 0, barbacued_chicken_show = "";
-let sushi_12_rolls_price    =  6, sushi_12_rolls_quantity    = 0, sushi_12_rolls_show    = "";
-let barbacue_price          = 30, barbacue_quantity          = 0, barbacue_show          = "";
-let hambuguer_price         = 10, hambuguer_quantity         = 0, hambuguer_show         = "";
+let barbacued_chicken_price = 20, barbacued_chicken_quantity = 0, barbacued_chicken_show = "", barbacued_chicken_result = 0;
+let sushi_12_rolls_price    =  6, sushi_12_rolls_quantity    = 0, sushi_12_rolls_show    = "", sushi_12_rolls_result    = 0;
+let barbacue_price          = 30, barbacue_quantity          = 0, barbacue_show          = "", barbacue_result          = 0;
+let hambuguer_price         = 10, hambuguer_quantity         = 0, hambuguer_show         = "", hambuguer_result         = 0;
 let BillStatement           = "";
 
 document.getElementById("name").addEventListener("keyup", function() {
@@ -14,10 +14,12 @@ document.getElementById("email").addEventListener("keyup", function() {
 
 document.getElementById("barbacued_chicken").addEventListener("keyup", function() {
     if(this.value == "" || this.value == 0) {
-        barbacued_chicken_show = "";
+        barbacued_chicken_show   = "";
+        barbacued_chicken_result = 0;
         ShowBill();
     } else {        
         barbacued_chicken_quantity = this.value;
+        barbacued_chicken_result   = barbacued_chicken_price * barbacued_chicken_quantity;
         barbacued_chicken_show     = `<tr><td>Barbecued chicken</td><td>${barbacued_chicken_price} USD</td><td>${barbacued_chicken_quantity}</td><td>${barbacued_chicken_price * barbacued_chicken_quantity} USD</td></tr>`;
         ShowBill();
     }
@@ -25,10 +27,12 @@ document.getElementById("barbacued_chicken").addEventListener("keyup", function(
 
 document.getElementById("sushi_12_rolls").addEventListener("keyup", function() {
     if(this.value == "" || this.value == 0) {
-        sushi_12_rolls_show = "";
+        sushi_12_rolls_show   = "";
+        sushi_12_rolls_result = 0;
         ShowBill();
     } else {        
         sushi_12_rolls_quantity = this.value;
+        sushi_12_rolls_result   = sushi_12_rolls_price * sushi_12_rolls_quantity;
         sushi_12_rolls_show     = `<tr><td>Sushi (12 Rolls)</td><td>${sushi_12_rolls_price} USD</td><td>${sushi_12_rolls_quantity}</td><td>${sushi_12_rolls_price * sushi_12_rolls_quantity} USD</td></tr>`;
         ShowBill();
     }    
@@ -36,10 +40,12 @@ document.getElementById("sushi_12_rolls").addEventListener("keyup", function() {
 
 document.getElementById("barbacue").addEventListener("keyup", function() {
     if(this.value == "" || this.value == 0) {
-        barbacue_show = "";
+        barbacue_show   = "";
+        barbacue_result = 0
         ShowBill();
     } else {        
         barbacue_quantity = this.value;
+        barbacue_result   = barbacue_price * barbacue_quantity;
         barbacue_show     = `<tr><td>Barbacue</td><td>${barbacue_price} USD</td><td>${barbacue_quantity}</td><td>${barbacue_price * barbacue_quantity} USD</td></tr>`;
         ShowBill();
     }
@@ -47,10 +53,12 @@ document.getElementById("barbacue").addEventListener("keyup", function() {
 
 document.getElementById("hambuguer").addEventListener("keyup", function() {
     if(this.value == "" || this.value == 0) {
-        hambuguer_show = "";
+        hambuguer_show   = "";
+        hambuguer_result = 0
         ShowBill();
     } else {        
         hambuguer_quantity = this.value;
+        hambuguer_result   = hambuguer_price * hambuguer_quantity;
         hambuguer_show     = `<tr><td>Hamburguer</td><td>${hambuguer_price} USD</td><td>${hambuguer_quantity}</td><td>${hambuguer_price * hambuguer_quantity} USD</td></tr>`;
         ShowBill();
     }
@@ -58,4 +66,5 @@ document.getElementById("hambuguer").addEventListener("keyup", function() {
 
 function ShowBill() {
     document.getElementById("BillStatement").innerHTML = barbacued_chicken_show + sushi_12_rolls_show + barbacue_show + hambuguer_show;
+    document.getElementById("amount").innerHTML        = barbacued_chicken_result + sushi_12_rolls_result + barbacue_result + hambuguer_result;
 }
