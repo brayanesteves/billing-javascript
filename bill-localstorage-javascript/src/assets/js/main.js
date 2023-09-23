b1231147f867d9e777efceedc7f810c558bead92
Brayan Esteves <brayan.esteves93@gmail.com>
2023-09-22 20:36:51 -0400

    Refactor Structure & Add constants [22/09/2023]

---
+++ bill-localstorage-javascript/src/assets/js/main.js
@@ -0,0 +1,9 @@
+const formDetail = document.getElementById("formDetail");
+
+const amount            = document.getElementById("amount");
+const selectDescription = document.getElementById("selectDescription");
+const priceUnit         = document.getElementById("priceUnit");
+const priceTotal        = document.getElementById("priceTotal");
+const add               = document.getElementById("add");
+
+let arrayDetail = [];
